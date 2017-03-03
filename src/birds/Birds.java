cfff74968833a87d7e96b41c50b0f70f0c6d7695
Ayush Sharma <ayush.cs.umass@gmail.com>
2017-03-03 16:53:28 -0500

    Bird Class completed

--- src/birds/Birds.java
+++ src/birds/Birds.java
@@ -3,18 +3,23 @@ package birds;
 import java.util.ArrayList;
 import java.util.List;
 
+import constants.Constants;
+import utils.Utils;
+
 public class Birds {
 	private int id;
 	private int duration;
 	private int destination;
 	
-	private void initialize() {
-		
+	public Birds() {
+		destination = Utils.getInRange(Constants.BirdConstants.MIN_DESTINATION, Constants.BirdConstants.MAX_DESTINATION);
+		duration = Utils.getInRange(Constants.BirdConstants.MIN_DURATION, Constants.BirdConstants.MAX_DURATION);
+		id = Constants.BirdConstants.DEFAULT_BIRD_ID;
 	}
 	
 	public List<Integer> getFlightDetails() {
-		initialize();
 		List<Integer> list = new ArrayList<Integer>();
+		list.add(id); list.add(duration); list.add(destination);
 		return list;
 	}
 }
