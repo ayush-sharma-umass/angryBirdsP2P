d7b8272d2fdde1086b2fe329b13a464e500d6826
Ayush Sharma <ayush.cs.umass@gmail.com>
2017-03-03 14:53:10 -0500

    First commit

---
+++ src/constants/Constants.java
@@ -0,0 +1,21 @@
+package constants;
+
+public class Constants {
+	
+	public static class BirdConstants {
+		// Number of birds in the game
+		public static final int NUMBER_OF_BIRDS = 3;
+	}
+	
+	public static class PigConstants {
+		// Max number of pigs in the game
+		public static final int MAX_PIGS = 6;
+		// Max neighbours of pigs
+		public static final int MAX_PIG_NEIGHBOURS = 2;
+	}
+	
+	public static class MapConstants {
+		// Size of maximum physical network
+		public static final int MAP_SIZE = 1000;
+	}
+}
