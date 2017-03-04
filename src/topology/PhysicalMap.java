cc4bb4f5b30261636eb840be62ecf3418d4fef3a
Ayush Sharma <ayush.cs.umass@gmail.com>
2017-03-03 19:07:48 -0500

    Physical Map class added

--- src/topology/PhysicalMap.java
+++ src/topology/PhysicalMap.java
@@ -7,36 +7,32 @@ import java.util.List;
 import java.util.Map;
 
 import constants.Constants;
-import utils.Utils;
 
-public class Network {
+public class PhysicalMap {
 	private int nPigs;
 	private Map<Integer, List<Integer>> map;
 	
-	static Network instance = null;
-	public static Network getInstance() {
-		if (instance == null) {
-			instance = new Network();
-		}
-		return instance;
-	}
-	
-	private Network() {
+	public PhysicalMap() {
 		// number of pigs
 		nPigs = Constants.PigConstants.NUM_PIGS;
 		map = new HashMap<Integer, List<Integer>>();
 		
-		// generate a network map
+		// generate a random network map
 		List<Integer> list = new ArrayList<Integer>();
 		for (int i = 0; i < nPigs; i++) {
-			int cur = i;
-			int prev = (nPigs + i -1)%nPigs;
-			int next = (nPigs + i + 1)%nPigs;
+			list.add(i);
+		}
+		// generate a random permutation of a list
+		Collections.shuffle(list);
+				
+		for (int i = 0; i < nPigs; i++) {
+			int cur = list.get(i);
+			int prev = list.get((nPigs + i-1)%nPigs);
+			int next = list.get((nPigs + i+1)%nPigs);
 			List<Integer> nbrList = new ArrayList<Integer>();
 			list.add(prev); list.add(next);
 			map.put(cur, nbrList);
-		}
-		
+		}	
 	}
 	
 	
